39e69a71b092ee0227763b6efe829340a6288b06
Edward Ruzek <edadiddles@hotmail.com>
2025-12-30 09:35:54 -0500

    adding load u64 big endian with simple test

--- src/endian.zig
+++ src/endian.zig
@@ -12,8 +12,16 @@ pub fn load_u32_le(b: []const u8) u32 {
             (@as(u32, b[1]) << 8) |
             (@as(u32, b[0]) << 0);
 }
-pub fn load_u64_be(bytes: []const u8) void {
-    _ = bytes;
+pub fn load_u64_be(b: []const u8) u64 {
+    return (@as(u64, b[0]) << 56) |
+            (@as(u64, b[1]) << 48) |
+            (@as(u64, b[2]) << 40) |
+            (@as(u64, b[3]) << 32) |
+            (@as(u64, b[4]) << 24) |
+            (@as(u64, b[5]) << 16) |
+            (@as(u64, b[6]) << 8) |
+            (@as(u64, b[7]) << 0);
+
 }
 pub fn load_u64_le(bytes: []const u8) void {
     _ = bytes;
@@ -46,6 +54,14 @@ test "load_u32_le basic" {
     try std.testing.expectEqual(
         @as(u32, 0x12345678),
         load_u32_le(&b),
+    );
+}
+
+test "load_u64_be basic" {
+    const b = [_]u8{ 0x12, 0x34, 0x56, 0x78, 0x90, 0xab, 0xcd, 0xef };
+    try std.testing.expectEqual(
+        @as(u64, 0x1234567890abcdef),
+        load_u64_be(&b),
     );
 }
 
