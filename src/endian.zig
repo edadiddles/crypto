8761f7efedc76b9c5178c4a2cacadffe9a401735
Edward Ruzek <edadiddles@hotmail.com>
2025-12-30 09:31:46 -0500

    adding load u32 little endian method with simple test

--- src/endian.zig
+++ src/endian.zig
@@ -6,8 +6,11 @@ pub fn load_u32_be(b: []const u8) u32 {
             (@as(u32, b[2]) << 8) |
             (@as(u32, b[3]) << 0);
 }
-pub fn load_u32_le(bytes: []const u8) void {
-    _ = bytes;
+pub fn load_u32_le(b: []const u8) u32 {
+    return (@as(u32, b[3]) << 24) |
+            (@as(u32, b[2]) << 16) |
+            (@as(u32, b[1]) << 8) |
+            (@as(u32, b[0]) << 0);
 }
 pub fn load_u64_be(bytes: []const u8) void {
     _ = bytes;
@@ -35,6 +38,14 @@ test "load_u32_be basic" {
     try std.testing.expectEqual(
         @as(u32, 0x12345678),
         load_u32_be(&b),
+    );
+}
+
+test "load_u32_le basic" {
+    const b = [_]u8{ 0x78, 0x56, 0x34, 0x12 };
+    try std.testing.expectEqual(
+        @as(u32, 0x12345678),
+        load_u32_le(&b),
     );
 }
 
