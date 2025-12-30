ea5d720576bc477b1138743fc1c97c7f78c2b58d
Edward Ruzek <edadiddles@hotmail.com>
2025-12-30 09:16:22 -0500

    adding new file to operate on endianess of byte layout

---
+++ src/endian.zig
@@ -0,0 +1,28 @@
+const std = @import ("std");
+
+pub fn load_u32_be(bytes: []const u8) void {
+    _ = bytes;
+}
+pub fn load_u32_le(bytes: []const u8) void {
+    _ = bytes;
+}
+pub fn load_u64_be(bytes: []const u8) void {
+    _ = bytes;
+}
+pub fn load_u64_le(bytes: []const u8) void {
+    _ = bytes;
+}
+
+pub fn store_u32_be() []u8 {
+    return []u8{};
+}
+pub fn store_u32_le() []u8 {
+    return []u8{};
+}
+pub fn store_u64_be() []u8 {
+    return []u8{};
+}
+pub fn store_u64_le() []u8 {
+    return []u8{};
+}
+
