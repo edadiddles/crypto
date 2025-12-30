f7886e26dff818ecd06c81be854f7fc3c1655b49
Edward Ruzek <edadiddles@hotmail.com>
2025-12-30 09:39:39 -0500

    add method to load u64 little endian with a simple test

--- src/endian.zig
+++ src/endian.zig
@@ -21,10 +21,16 @@ pub fn load_u64_be(b: []const u8) u64 {
             (@as(u64, b[5]) << 16) |
             (@as(u64, b[6]) << 8) |
             (@as(u64, b[7]) << 0);
-
 }
-pub fn load_u64_le(bytes: []const u8) void {
-    _ = bytes;
+pub fn load_u64_le(b: []const u8) u64 {
+    return (@as(u64, b[7]) << 56) |
+            (@as(u64, b[6]) << 48) |
+            (@as(u64, b[5]) << 40) |
+            (@as(u64, b[4]) << 32) |
+            (@as(u64, b[3]) << 24) |
+            (@as(u64, b[2]) << 16) |
+            (@as(u64, b[1]) << 8) |
+            (@as(u64, b[0]) << 0);
 }
 
 pub fn store_u32_be() []u8 {
@@ -62,6 +68,14 @@ test "load_u64_be basic" {
     try std.testing.expectEqual(
         @as(u64, 0x1234567890abcdef),
         load_u64_be(&b),
+    );
+}
+
+test "load_u64_le basic" {
+    const b = [_]u8{ 0xef, 0xcd, 0xab, 0x90, 0x78, 0x56, 0x34, 0x12 };
+    try std.testing.expectEqual(
+        @as(u64, 0x1234567890abcdef),
+        load_u64_le(&b),
     );
 }
 
