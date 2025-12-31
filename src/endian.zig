0cae95531edad5093bbf820bc29fcfc6de93d097
Edward Ruzek <edadiddles@hotmail.com>
2025-12-31 15:03:49 -0500

    adding edge case tests for store_u32_le

--- src/endian.zig
+++ src/endian.zig
@@ -292,6 +292,43 @@ test "store_u32_le basic" {
     );
 }
 
+test "store_u32_le edge cases" {
+    var b0 = [_]u8 { 0x00, 0x00, 0x00, 0x00 };
+    store_u32_le(&b0, 0x00000000);
+    try std.testing.expectEqual(
+        [_]u8 { 0x00, 0x00, 0x00, 0x00 },
+        b0,
+    );
+    
+    var b1 = [_]u8 { 0x00, 0x00, 0x00, 0x00 };
+    store_u32_le(&b1, 0xffffffff);
+    try std.testing.expectEqual(
+        [_]u8 { 0xff, 0xff, 0xff, 0xff },
+        b1,
+    );
+
+    var b2 = [_]u8 { 0x00, 0x00, 0x00, 0x00 };
+    store_u32_le(&b2, 0xaa55aa55);
+    try std.testing.expectEqual(
+        [_]u8 { 0x55, 0xaa, 0x55, 0xaa },
+        b2,
+    );
+    
+    var b3 = [_]u8 { 0x00, 0x00, 0x00, 0x00 };
+    store_u32_le(&b3, 0x55aa55aa);
+    try std.testing.expectEqual(
+        [_]u8 { 0xaa, 0x55, 0xaa, 0x55 },
+        b3,
+    );
+    
+    var b4 = [_]u8 { 0x00, 0x00, 0x00, 0x00, 0x00 };
+    store_u32_le(&b4, 0x12345678);
+    try std.testing.expectEqual(
+        [_]u8 { 0x78, 0x56, 0x34, 0x12, 0x00 },
+        b4,
+    );
+}
+
 test "store_u64_be basic" {
     var b = [_]u8 { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
     store_u64_be(&b, 0x1234567890abcdef);
