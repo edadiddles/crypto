9e3e698e70d3f931e1215b9f1155a21e7c95d18a
Edward Ruzek <edadiddles@hotmail.com>
2025-12-31 08:03:54 -0500

    adding edge case tests for load_u32_le

--- src/endian.zig
+++ src/endian.zig
@@ -100,6 +100,38 @@ test "load_u32_le basic" {
     );
 }
 
+test "load_u32_le edge cases" {
+    const b0 = [_]u8{ 0x00, 0x00, 0x00, 0x00 };
+    try std.testing.expectEqual(
+        @as(u32, 0x00000000),
+        load_u32_le(&b0),
+    );
+
+    const b1 = [_]u8{ 0xff, 0xff, 0xff, 0xff };
+    try std.testing.expectEqual(
+        @as(u32, 0xffffffff),
+        load_u32_le(&b1),
+    );
+
+    const b2 = [_]u8{ 0xaa, 0x55, 0xaa, 0x55 };
+    try std.testing.expectEqual(
+        @as(u32, 0x55aa55aa),
+        load_u32_le(&b2),
+    );
+
+    const b3 = [_]u8{ 0x55, 0xaa, 0x55, 0xaa };
+    try std.testing.expectEqual(
+        @as(u32, 0xaa55aa55),
+        load_u32_le(&b3),
+    );
+
+    const b4 = [_]u8{ 0x12, 0x34, 0x56, 0x78, 0x90 };
+    try std.testing.expectEqual(
+        @as(u32, 0x78563412),
+        load_u32_le(&b4),
+    );
+}
+
 test "load_u64_be basic" {
     const b = [_]u8{ 0x12, 0x34, 0x56, 0x78, 0x90, 0xab, 0xcd, 0xef };
     try std.testing.expectEqual(
