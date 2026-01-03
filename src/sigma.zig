e68a9b7eb446ecc4e3b9090686920359117141f6
Edward Ruzek <edadiddles@hotmail.com>
2026-01-03 07:57:28 -0500

    stubbing out the sigma functions before implementing

---
+++ src/sigma.zig
@@ -0,0 +1,13 @@
+const std = @import("std");
+const bitops = @import("bitops.zig");
+
+
+pub fn small_sigma0_32(x: u32) u32 { return x; }
+pub fn small_sigma1_32(x: u32) u32 { return x; }
+pub fn big_sigma0_32(x: u32) u32 { return x; }
+pub fn big_sigma1_32(x: u32) u32 { return x; }
+
+pub fn small_sigma0_64(x: u64) u64 { return x; }
+pub fn small_sigma1_64(x: u64) u64 { return x; }
+pub fn big_sigma0_64(x: u64) u64 { return x; }
+pub fn big_sigma1_64(x: u64) u64 { return x; }
