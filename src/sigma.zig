3849587cb59cab9581976a3b9eb8e3ab3e6527cc
Edward Ruzek <edadiddles@hotmail.com>
2026-01-04 08:56:15 -0500

    adding 32bit implementations for big_sigma0 and big_sigma1

--- src/sigma.zig
+++ src/sigma.zig
@@ -9,8 +9,13 @@ pub fn small_sigma1_32(x: u32) u32 {
     return bitops.rotr32(x, 17) ^ bitops.rotr32(x, 19) ^ x >> 10;
 }
 
-pub fn big_sigma0_32(x: u32) u32 { return x; }
-pub fn big_sigma1_32(x: u32) u32 { return x; }
+pub fn big_sigma0_32(x: u32) u32 {
+    return bitops.rotr32(x, 2) ^ bitops.rotr32(x, 13) ^ bitops.rotr32(x, 22);
+}
+
+pub fn big_sigma1_32(x: u32) u32 {
+    return bitops.rotr32(x, 6) ^ bitops.rotr32(x, 11) ^ bitops.rotr32(x, 25);
+}
 
 pub fn small_sigma0_64(x: u64) u64 { return x; }
 pub fn small_sigma1_64(x: u64) u64 { return x; }
