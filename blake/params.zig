f0fbb2d44ea5fe3c2c2514a9530767f7671746da
Edward Ruzek <edadiddles@hotmail.com>
2026-01-15 15:41:36 -0500

    adding init methods for Blake2s param and state structs

--- blake/params.zig
+++ blake/params.zig
@@ -1,7 +1,11 @@
-pub const IV: [8]u32 = undefined;
+pub const IV: [8]u32 = .{
+    0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
+    0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19,
+};
+
 pub const SIGMA: [10][16]u8 = undefined;
 
-pub const Params = struct {
+pub const Blake2sParams = packed struct {
     digest_length: u8,
     key_length: u8,
     fanout: u8,
@@ -10,4 +14,24 @@ pub const Params = struct {
     node_offset: u64,
     node_depth: u8,
     inner_length: u8,
+    reserved: [14]u8,
+
+    pub fn init(digest_len: u8, key_len: u8) Blake2sParams {
+        return .{
+            .digest_length = digest_len,
+            .key_length = key_len,
+            .fanout = 1,
+            .depth = 1,
+            .leaf_length = 0,
+            .node_offset = 0,
+            .node_depth = 0,
+            .inner_length = 0,
+            .reserved = [_]u8{0}**14,
+        };
+    }
+
+    pub fn as_words(p: *const Blake2sParams) *const [8]u32 {
+        return @ptrCast(p);
+    }
 };
+
--- blake/state.zig
+++ blake/state.zig
@@ -1,7 +1,25 @@
+const params = @import("params.zig");
+
+const BLOCK_SIZE = 64;
+
 pub const Blake2sState = struct {
     h: [8]u32,
     t: u64,
-    buf: [64]u8,
+    f: u64,
+    buf: [BLOCK_SIZE]u8,
     buf_len: usize,
-    is_finalized: bool,
+    out_len: u8,
+
+    fn init(self: *Blake2sState, blake2sParams: *const params.Blake2sParams) void {
+        const param_words = blake2sParams.as_words();
+        for(0..8) |i| {
+            self.h[i] = params.IV[i] ^ param_words[i];
+        }
+
+        self.t = 0;
+        self.f = 0;
+        self.buf = .{0}**BLOCK_SIZE;
+        self.buf_len = 0;
+        self.out_len = blake2sParams.digest_length;
+    }
 };
