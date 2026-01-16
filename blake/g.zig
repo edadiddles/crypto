99cdfedd85548f0cf5ce786c40dd4a5f34f929a6
Edward Ruzek <edadiddles@hotmail.com>
2026-01-16 09:24:59 -0500

    defining the g function

--- blake/g.zig
+++ blake/g.zig
@@ -1,8 +1,13 @@
+const rotr = @import("../src/bitops.zig").rotr32;
+const R = [_]u32{16, 12, 8, 7};
+
 pub fn G(a: *u32, b: *u32, c: *u32, d: *u32, x: *u32, y: *u32) void {
-    _ = a;
-    _ = b;
-    _ = c;
-    _ = d;
-    _ = x;
-    _ = y;
+    a = a + b + x;
+    d = rotr(d ^ a,  R[0]);
+    c = c + d;
+    b = rotr(b ^ c, R[1]);
+    a = a + b + y;
+    d = rotr(d ^ a, R[2]);
+    c = c + d;
+    b = rotr(b ^ c, R[3]);
 }
