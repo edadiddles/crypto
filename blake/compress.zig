886314884cd6f84d11465591d6a6eec3f9eee6af
Edward Ruzek <edadiddles@hotmail.com>
2026-01-16 08:34:47 -0500

    trying to write the round function from the conceptual outline provided by chatgpt

--- blake/compress.zig
+++ blake/compress.zig
@@ -1,5 +1,7 @@
 const IV = @import("params.zig").IV;
 const Blake2sState = @import("state.zig").Blake2sState;
+const G = @import("g.zig").G;
+const SIGMA = @import("params.zig").SIGMA;
 
 const ROUNDS = 10;
 
@@ -40,9 +42,15 @@ fn initWorkVector(state: *Blake2sState, v: [16]u32) void {
 }
 
 fn round(v: [16]u32, m: [16]u32, r: usize) void {
-    _ = v;
-    _ = m;
-    _ = r;
+    G(v[0], v[4], v[6], v[12], m[SIGMA[r][0]], m[SIGMA[r][1]]);
+    G(v[1], v[5], v[9], v[13], m[SIGMA[r][2]], m[SIGMA[r][3]]);
+    G(v[2], v[6], v[10], v[14], m[SIGMA[r][4]], m[SIGMA[r][5]]);
+    G(v[3], v[7], v[11], v[15], m[SIGMA[r][6]], m[SIGMA[r][7]]);
+
+    G(v[0], v[5], v[10], v[15], m[SIGMA[r][8]], m[SIGMA[r][9]]);
+    G(v[1], v[6], v[11], v[12], m[SIGMA[r][10]], m[SIGMA[r][11]]);
+    G(v[2], v[7], v[8], v[13], m[SIGMA[r][12]], m[SIGMA[r][13]]);
+    G(v[3], v[4], v[9], v[14], m[SIGMA[r][14]], m[SIGMA[r][15]]);
 }
 
 fn feedForward(state: *Blake2sState, v: [16]u32) void {
